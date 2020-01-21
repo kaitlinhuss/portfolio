f482912f9f2ecec24bad3fec5a196aa3b25c853c
Kaitlin Huss <kaitlinhuss@gmail.com>
2020-01-20 21:43:25 -0500

    Update formatting for sections.

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,9 @@ import Nav from 'react-bootstrap/Nav'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaArrowDown } from 'react-icons/fa';
 
+import scrollTo from 'gatsby-plugin-smoothscroll';
 
 class About extends Section {
     render() {
@@ -15,14 +17,14 @@ class About extends Section {
             style={{
             margin: `0 auto`,
             maxWidth: 2000,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: `10rem 1.0875rem 10rem`,
             paddingTop: 0,
             }}
         >
-          <p id="about"><br></br></p>
+          <p id="about"></p>
           <h1>about</h1>
           <hr class="lg-divider"/>
-          <p id="about">A science-fiction nerd with a slight obsession for Stephen King novels.<br/>
+          <p id="about-text">A science-fiction nerd with a slight obsession for Stephen King novels.<br/>
             When I'm not hiking, I'm exploring new restaurants downtown. <br/>
             I write code for a living, but I'm also passionate about writing short stories and photography.</p>
             <hr class="sm-divider"/>
@@ -54,18 +56,25 @@ class About extends Section {
                       As I grow in my career, I search for opportunities that allow me to design and develop software solutions for challenges of all shapes and sizes.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                    <p class="tab-content-two"><b>Languages/Frameworks</b>: Proficient in HTML, CSS, Java; experienced with Groovy, Maven; familiar with JavaScript, JUnit; previously used AngularJS, Spring; currently learning React, Cucumber, Selenium, GoLang.<br/><br/>
-                <b>Technologies/Tools:</b> Proficient in JIRA, Confluence, Bitbucket, Git; experienced with GitHub, IntelliJ; familiar with Jenkins; previously used SQL; currently learning Docker, Kubernetes, Spinnaker, JFrog Artifactory.</p>
+                    <p class="tab-content-two"><b>Languages/Frameworks</b>: <br/>
+                    Proficient in HTML, CSS, Java; experienced with Groovy, Maven; familiar with JavaScript, JUnit; previously used AngularJS, Spring; currently learning React, Cucumber, Selenium, GoLang.<br/><br/>
+                <b>Technologies/Tools:</b> <br/>
+                Proficient in JIRA, Confluence, Bitbucket, Git; experienced with GitHub, IntelliJ; familiar with Jenkins; previously used SQL; currently learning Docker, Kubernetes, Spinnaker, JFrog Artifactory.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                    <p class="tab-content-three">Currently, I'm learning to construct automated continuous integration &amp; delivery workflows for application teams across the firm, including containerizing 
-applications with Docker and deploying them to internal and external cloud environments, using Jenkins, Spinnaker, Kubernetes, and Artifactory.</p>
+                    <p class="tab-content-three">Currently, I'm learning to construct automated continuous integration &amp; delivery workflows for application teams across the company, including containerizing 
+applications with Docker and deploying them to internal and external cloud environments using Jenkins, Spinnaker, Kubernetes, and Artifactory. During this year, I plan to continue expanding my experience with technologies needed to build rich and complex user interfaces, 
+including React and SASS. I'm excited about diving into the world of API design and development within the next few months.</p>
                     </Tab.Pane>
                   </Tab.Content>
                 </Col>
               </Row>
             </Tab.Container>
+            <div className="container">
+              <button class="btn btn-outline-primary scrolldown-btn" onClick={() => scrollTo('#code')}><FaArrowDown /></button>
+            </div>
         </div>
+        
       )
     }
   }
